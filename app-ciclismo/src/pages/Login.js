1b0989924f1f1f346421efbd4a3bd3601e3ed34d
unknown <ivanildojunior2319@gmail.com>
2023-08-24 20:36:30 -0300

    login page ready

--- app-ciclismo/src/pages/Login.js
+++ app-ciclismo/src/pages/Login.js
@@ -1,10 +1,34 @@
+import { useState } from 'react';
 import '../pages-css/Login.css';
+import { Link } from 'react-router-dom';
 
 function Login() {
-    return (
-        <p className="text">Página de Login</p>
 
-        /* Aqui é o login. Muito fácil. Formulário pedindo email e senha, o resto é cachorro frio e suco quente, pode deixar cmg :)    */
+    const [loginState, setLoginState] = useState({
+        email: '',
+        password: ''
+    })
+
+    const handleOnChangeLogin = (e, key) => {
+        setLoginState({...loginState, [key]: e.target.value})
+    }
+
+    const handleLoginForm = (e) => {
+        e.preventDefault()
+        console.log(loginState)
+    }
+
+    return (
+        <div className='container-login'>
+            <form className='form' onSubmit={handleLoginForm}>
+                <h1>Bem Vindo!</h1>
+                <input type='email' placeholder='E-mail' required value={loginState.email} onChange={(e) => handleOnChangeLogin(e, 'email')}/>
+                <input type='password' placeholder='Senha' required value={loginState.password} onChange={(e) => handleOnChangeLogin(e, 'password')} />
+                <button type='submit'>Login</button> 
+                <p>Não tem uma conta? <Link to='/cadastro'>Cadastre-se</Link></p>           
+            </form>
+            
+        </div>    
     )
 }
 
