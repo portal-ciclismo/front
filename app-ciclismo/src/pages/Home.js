d4f976050f20b5596ebd2175904a287eb22d62e5
kassioL2L <kassio_leite2@hotmail.com>
2023-08-31 20:35:09 -0300

    adicionando a estrutura do segundo sprint

--- app-ciclismo/src/pages/Home.js
+++ app-ciclismo/src/pages/Home.js
@@ -1,13 +1,32 @@
+import React from 'react';
 import '../pages-css/Home.css';
 
-function Home() {
-    return (
-        <main>
-            <p className="text">Página principal!</p>
-        </main>
-
-        /* A tela inicial é aqui. Onde vai ter as coisa lá da tela inicial. Temos que decidir os componentes e o que vai estar aqui!  :)   */
-    )
-}
+const Home = () => {
+  return (
+    <div className="home-container">
+      <section className="section1">
+        {/* Conteúdo da primeira seção */}
+      </section>
+      <section className="section2">
+        {/* Conteúdo da segunda seção */}
+      </section>
+      <section className="section3">
+        {/* Conteúdo da terceira seção */}
+      </section>
+      <section className="section4">
+        {/* Conteúdo da quarta seção */}
+      </section>
+      <section className="section5">
+        {/* Conteúdo da quinta seção */}
+      </section>
+      <section className="section6">
+        {/* Conteúdo da sexta seção */}
+        <footer>
+          {/* Conteúdo do footer */}
+        </footer>
+      </section>
+    </div>
+  );
+};
 
 export default Home;
