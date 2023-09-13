a710d72c7062e764955d1a7fd402ac309ccb1909
Timmers Arruda <31082154+timmersarruda@users.noreply.github.com>
2023-09-12 23:11:17 -0300

    Criacao e estilizacao da section3 com adicao do componente CategoriaBox

--- app-ciclismo/src/pages/Home.js
+++ app-ciclismo/src/pages/Home.js
@@ -2,6 +2,11 @@ import React from 'react';
 import '../pages-css/Home.css';
 import RecomendacaoBox from '../components/RecomendacaoBox';
 import CriarContaButton from '../components/CriarContaButton';
+import CategoriaBox from '../components/CategoriaBox';
+
+import Passeios from '../assets/background-imgs/medium-shot-family-cycling-outdoors.jpg';
+import Competicoes from '../assets/background-imgs/professional-cyclist-womeneee.jpg';
+import Masketplace from '../assets/background-imgs/close-up-young-businessman-bike-shop.jpg';
 
 import Aspas from '../assets/Icons/aspas_icon.jpg';
 import Perfil1 from '../assets/perfil_imgs/Rodrigo_perfil.jpg'
@@ -18,9 +23,9 @@ const Home = () => {
       <section className="section1">
         {/* Conteúdo da primeira seção */}
       </section>
-      
+
       <section className="section2">
-        <div className='section2-img'><img src={EstrelaColor}/></div>
+        <div className='section2-img'><img src={EstrelaColor} /></div>
 
         <div className='section2-container'>
 
@@ -37,6 +42,25 @@ const Home = () => {
       </section>
       <section className="section3">
         {/* Conteúdo da terceira seção */}
+        <span className="title-section3">Tudo o que você <br />precisa em um só lugar.</span>
+        <div className="container-categorias">
+          <CategoriaBox
+            textoCategoria="Passeios"
+            imagemCategoria={Passeios}
+          />
+          <CategoriaBox
+            textoCategoria="Competições"
+            imagemCategoria={Competicoes}
+          />
+          <CategoriaBox
+            textoCategoria="Marketplace"
+            imagemCategoria={Masketplace}
+          />
+        </div>
+        <div className="section3-button">
+          <CriarContaButton />
+        </div>
+
       </section>
       <section className="section4">
         {/* Conteúdo da quarta seção */}
@@ -45,44 +69,44 @@ const Home = () => {
         {/* Conteúdo da quinta seção */}
         <span className="title-section5">O que os camelers estão dizendo</span>
         <div className="container-recomendacao">
-        <RecomendacaoBox
-          aspas={Aspas}
-          texto="Adorei! Depois que me mudei para a capital, era difícil andar de bicicleta. Agora pedalo toda semana!"
-          imagemPerfil={Perfil1}
-          nome="Rodrigo, (professor)"
-        />
-        <RecomendacaoBox
-          aspas={Aspas}
-          texto="Tinha medo de pedalar sozinhao Agora saio em grupo com gente maravilhoso que me ajuda muito."
-          imagemPerfil={Perfil2}
-          nome="Vinicius, (FilhoDeRodrigo)"
-        />
-        <RecomendacaoBox
-          aspas={Aspas}
-          texto="Este Sensacional! Fiz um network que me ajuda a achar peças e serviços para minha bike de competição. Recomendo! um exemplo de recomendação."
-          imagemPerfil={Perfil3}
-          nome="Gabriel, (Escrivão)"
-        />
+          <RecomendacaoBox
+            aspas={Aspas}
+            texto="Adorei! Depois que me mudei para a capital, era difícil andar de bicicleta. Agora pedalo toda semana!"
+            imagemPerfil={Perfil1}
+            nome="Rodrigo, (professor)"
+          />
+          <RecomendacaoBox
+            aspas={Aspas}
+            texto="Tinha medo de pedalar sozinhao Agora saio em grupo com gente maravilhoso que me ajuda muito."
+            imagemPerfil={Perfil2}
+            nome="Vinicius, (FilhoDeRodrigo)"
+          />
+          <RecomendacaoBox
+            aspas={Aspas}
+            texto="Este Sensacional! Fiz um network que me ajuda a achar peças e serviços para minha bike de competição. Recomendo! um exemplo de recomendação."
+            imagemPerfil={Perfil3}
+            nome="Gabriel, (Escrivão)"
+          />
         </div>
-      
+
       </section>
       <section className="section6">
         <div className='section6-text1'>
-          <h1>Comunidade,<br/>Esporte,<br/> Lazer.</h1>
+          <h1>Comunidade,<br />Esporte,<br /> Lazer.</h1>
         </div>
         <div className='section6-text2'>
-          <h2>Tudo isso<br/>e muito mais.</h2>
+          <h2>Tudo isso<br />e muito mais.</h2>
         </div>
         <div className='section6-button'>
-          <CriarContaButton/>
+          <CriarContaButton />
         </div>
         <footer>
-            <img src={Logo} alt='Logo camelo' className='footer-img'/>
+          <img src={Logo} alt='Logo camelo' className='footer-img' />
           <div className='footer-text1'>
             <h1>FALE CONOSCO</h1>
           </div>
           <div className='footer-text2'>
-            <h1>Instagram<br/>Twitter<br/>Facebook<br/>LinkedIn</h1>
+            <h1>Instagram<br />Twitter<br />Facebook<br />LinkedIn</h1>
           </div>
           <div className='footer-text3'>
             <h1>contato@camelo.com</h1>
