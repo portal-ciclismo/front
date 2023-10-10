c5ba2803140d0df88f33c75874981b91ff6f4c03
Filipe Alves <filipe.alves@dcx.ufpb.br>
2023-10-10 19:12:15 -0300

    redirecionamento de button

--- app-ciclismo/src/components/TopBar/TopBar.js
+++ app-ciclismo/src/components/TopBar/TopBar.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import './TopBar.css';
 
 function TopBar() {
+  const usuario = "José Luiz Datena (Zé da Manga)";
+  const nivelAmador = "Nível 2"; // Nível de amador do usuário
+
   return (
     <div className="top-bar">
-      {/* Adicione a barra de pesquisa e o menu aqui */}
-      <input type="text" placeholder="Pesquisar" />
+      {/* Menu */}
       <nav>
         <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
+          <li className="menu-item">
+            <div className="search-bar-container">
+              <input type="text" placeholder="Pesquisar" className="search-bar" style={{ marginRight: 'auto' }} />
+            </div>
+          </li>
+          <li className="menu-item"></li>
           {/* Adicione mais itens de menu, se necessário */}
         </ul>
       </nav>
+
+      {/* Informações do perfil */}
       <div className="user-info">
-        <h2>Nome do Usuário</h2>
+        <h2>{usuario}</h2>
+        <p>Amador: {nivelAmador}</p>
         {/* Outras informações do perfil */}
       </div>
     </div>
