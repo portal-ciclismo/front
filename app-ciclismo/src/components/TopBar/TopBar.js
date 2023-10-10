7af4a2665c5e1c19b659d7d3b7dd9e6f06b2ab26
Cinthia Lindolfo <cinthia20lindolfo@gmail.com>
2023-10-10 20:22:15 -0300

    Criação do topbar parte2 feita com sucesso

--- app-ciclismo/src/components/TopBar/TopBar.js
+++ app-ciclismo/src/components/TopBar/TopBar.js
@@ -1,20 +1,38 @@
 import React from 'react';
+import './TopBar.css';
+import logo from '../../assets/Logo_Horizontal/logo_horizontal_branco.png';
+import Estrela from '../../assets/Estrelas/Estrela_grande_branca.png';
 
 function TopBar() {
+  const usuario = "José Luiz Datena (Zé da Manga)";
+  const nivelAmador = "Nível 2"; 
+
   return (
     <div className="top-bar">
-      {/* Adicione a barra de pesquisa e o menu aqui */}
-      <input type="text" placeholder="Pesquisar" />
+      {/* Menu */}
       <nav>
         <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
+          <li className="menu-item">
+            <div className="search-bar-container">
+              <img src={logo} alt="Logo1" />
+              <input type="text" placeholder="O que está procurando hoje?" className="search-bar" />
+              <button className="right-button">Buscar</button>
+            </div>
+          </li>
+          <li className="menu-item"></li>
           {/* Adicione mais itens de menu, se necessário */}
         </ul>
       </nav>
+      {/* Informações do perfil */}
       <div className="user-info">
-        <h2>Nome do Usuário</h2>
-        {/* Outras informações do perfil */}
+        <div className="info-container">
+          <img src={Estrela} alt="estrela_branca" />
+          <h2>
+            {usuario}
+          </h2>
+          <p>Amador: {nivelAmador}</p>
+          {/* Outras informações do perfil */}
+        </div>
       </div>
     </div>
   );
