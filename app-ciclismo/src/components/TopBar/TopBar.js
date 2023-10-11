ad20cda0117c1a45d5da0a5e10ce9dd9933f4291
João Silva <cinthia20lindolfo@gmail.com>
2023-10-11 13:09:07 -0300

    Butao e barra de pesquisa ajustados

--- app-ciclismo/src/components/TopBar/TopBar.js
+++ app-ciclismo/src/components/TopBar/TopBar.js
@@ -3,36 +3,28 @@ import './TopBar.css';
 import logo from '../../assets/Logo_Horizontal/logo_horizontal_branco.png';
 import Estrela from '../../assets/Estrelas/Estrela_grande_branca.png';
 
+
 function TopBar() {
-  const usuario = "José Luiz Datena (Zé da Manga)";
-  const nivelAmador = "Nível 2"; 
+  const usuario = "Rodrigo Rebouças (Professor)";
+  const nivelAmador = "Nível 2";
 
   return (
     <div className="top-bar">
-      {/* Menu */}
-      <nav>
-        <ul>
-          <li className="menu-item">
-            <div className="search-bar-container">
-              <img src={logo} alt="Logo1" />
-              <input type="text" placeholder="O que está procurando hoje?" className="search-bar" />
-              <button className="right-button">Buscar</button>
-            </div>
-          </li>
-          <li className="menu-item"></li>
-          {/* Adicione mais itens de menu, se necessário */}
-        </ul>
-      </nav>
+      <div className="menu-item-conteiner">
+        <img src={logo} alt="Logo1" />
+        <div className="search-container">
+          <input type="text" placeholder="  O que está procurando hoje?" />
+          <i className="search-icon"></i>
+        </div>
+        <button>Buscar</button>
+      </div>
 
-      {/* Informações do perfil */}
       <div className="user-info">
-      <div className="info-container">
-        <img src={Estrela} alt="estrela_branca" />
-          <h2>
-            {usuario}
-          </h2>
+        <div className="info-container">
+          <img src={Estrela} alt="estrela_branca" />
+          <h2>{usuario}</h2>
           <p>Amador: {nivelAmador}</p>
-          {/* Outras informações do perfil */}
+  
         </div>
       </div>
     </div>
