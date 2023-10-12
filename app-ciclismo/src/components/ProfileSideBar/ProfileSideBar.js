a01efbe8a9317b7580d6404cf844662203d0ad70
kassioL2L <kassio_leite2@hotmail.com>
2023-10-12 01:22:22 -0300

    refatorando profileSideBar

--- app-ciclismo/src/components/ProfileSideBar/ProfileSideBar.js
+++ app-ciclismo/src/components/ProfileSideBar/ProfileSideBar.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import "./ProfileSideBar.css";
-import profileImage from "../../assets/perfil_imgs/placeholderImage.png";
+import React, { useState } from 'react';
+import './ProfileSideBar.css';
+import profileImage from '../../assets/perfil_imgs/placeholderImage.png';
 
 function ProfileSidebar() {
+  const [bikeSectionOpen, setBikeSectionOpen] = useState(false);
+
+  const toggleBikeSection = () => {
+    setBikeSectionOpen(!bikeSectionOpen);
+  };
+
   return (
     <div className="profile-sidebar">
       <div className="profileImg">
         <img src={profileImage} alt="Foto de perfil" />
       </div>
-      <h1>
-        Grupos
-        <br />
-        Passeios
-        <br />
-        Contato
-        <br />
-        Reportar
-      </h1>
+      <ul>
+        <li>
+          <a href="#">Grupos</a>
+        </li>
+        <li>
+          <a href="#">Passeios</a>
+        </li>
+        <li>
+          <a href="#">Contato</a>
+        </li>
+        <li>
+          <a href="#">Reportar</a>
+        </li>
+        <li>
+          <div className="bike-section-toggle" onClick={toggleBikeSection}>
+            Bicicletas {bikeSectionOpen ? '-' : '+'}
+          </div>
+          {bikeSectionOpen && (
+            <ul className="bike-section">
+              <li>
+                <a href="#">Cadastre Sua Bike</a>
+              </li>
+              <li>
+                <a href="#">Visualize Sua Bike</a>
+              </li>
+            </ul>
+          )}
+        </li>
+      </ul>
     </div>
   );
 }
