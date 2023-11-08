ae53f5f33db2d9b81b9191f6971bd2e26c224ca4
kassioL2L <kassio_leite2@hotmail.com>
2023-11-08 08:47:57 -0300

    usando formik

--- app-ciclismo/src/pages/CadastroPerfil.js
+++ app-ciclismo/src/pages/CadastroPerfil.js
@@ -1,41 +1,47 @@
 import React, { useState } from 'react';
-import '../pages/CadastroPerfil';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import api from '../services/api';
+import { useNavigate } from 'react-router-dom';
 import BarraDePesquisa from '../components/BarraDePesquisa';
 
 function CadastroPerfil() {
-  const [profileData, setProfileData] = useState({
+  const navigate = useNavigate();
+
+  const initialProfileData = {
     fullName: '',
     nickname: '',
     sexo: '',
-    cyclingCategory: '',
+    cyclingCategory: 'AMADOR',
     location: '',
     profilePicture: null,
-  });
-
-  const handleChange = (e) => {
-    const { name, value, type, files } = e.target;
-    if (type === 'file') {
-      const selectedFile = files[0];
-      if (selectedFile) {
-        if (['image/jpeg', 'image/jpg', 'image/png', 'image/gif'].includes(selectedFile.type)) {
-          setProfileData({
-            ...profileData,
-            [name]: selectedFile,
-          });
-        } else {
-          alert('Por favor, selecione um arquivo de imagem válido.');
-        }
-      }
-    } else {
-      setProfileData({
-        ...profileData,
-        [name]: value,
-      });
-    }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const profileSchema = Yup.object().shape({
+    fullName: Yup.string().required('Campo obrigatório'),
+    nickname: Yup.string().required('Campo obrigatório'),
+    sexo: Yup.string().required('Campo obrigatório'),
+    cyclingCategory: Yup.string().required('Campo obrigatório'),
+    location: Yup.string().required('Campo obrigatório'),
+    profilePicture: Yup.mixed(),
+  });
+
+  const handleSubmit = (values) => {
+    const formData = new FormData();
+    formData.append('fullName', values.fullName);
+    formData.append('nickname', values.nickname);
+    formData.append('sexo', values.sexo);
+    formData.append('cyclingCategory', values.cyclingCategory);
+    formData.append('location', values.location);
+    formData.append('profilePicture', values.profilePicture);
+
+    api.post('/profile', formData)
+      .then((response) => {
+        navigate('/perfil');
+      })
+      .catch((error) => {
+        navigate('/perfil');
+      });
   };
 
   return (
@@ -43,85 +49,62 @@ function CadastroPerfil() {
       <BarraDePesquisa />
 
       <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <div className="form-cadastro-perfil">
-            <label className="form-label" htmlFor="fullName">
-              Nome Completo
-            </label>
-            <input
-              type="text"
-              name="fullName"
-              value={profileData.fullName}
-              onChange={handleChange}
-              className="form-input"
-            />
-          </div>
-          <div className="form-cadastro-perfil">
-            <label className="form-label" htmlFor="nickname">
-              Apelido
-            </label>
-            <input
-              type="text"
-              name="nickname"
-              value={profileData.nickname}
-              onChange={handleChange}
-              className="form-input"
-            />
-          </div>
-          <div className="form-cadastro-perfil">
-            <label className="form-label" htmlFor="sexo">
-              Sexo
-            </label>
-            <input
-              type="text"
-              name="sexo"
-              value={profileData.sexo}
-              onChange={handleChange}
-              className="form-input"
-            />
-          </div>
-          <div className="form-cadastro-perfil">
-            <label className="form-label" htmlFor="cyclingCategory">
-              Categoria de Ciclismo
-            </label>
-            <select
-              name="cyclingCategory"
-              value={profileData.cyclingCategory}
-              onChange={handleChange}
-              className="form-input"
-            >
-              <option value="AMADOR">Amador</option>
-              <option value="PROFISSIONAL">Profissional</option>
-            </select>
-          </div>
-          <div className="form-cadastro-perfil">
-            <label className="form-label" htmlFor="location">
-              Localização
-            </label>
-            <input
-              type="text"
-              name="location"
-              value={profileData.location}
-              onChange={handleChange}
-              className="form-input"
-            />
-          </div>
-          <div className="form-cadastro-perfil">
-            <label className="form-label" htmlFor="profilePicture">
-              Foto de Perfil
-            </label>
-            <input
-              type="file"
-              name="profilePicture"
-              accept="image/jpeg, image/jpg, image/png, image/gif"
-              onChange={handleChange}
-              className="form-input"
-            />
-          </div>
-          <button type="submit" className="form-button">
-            Salvar Perfil
-          </button>
-        </form>
+        <Formik
+          initialValues={initialProfileData}
+          validationSchema={profileSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <div className="form-cadastro-perfil">
+              <label className="form-label" htmlFor="fullName">
+                Nome Completo
+              </label>
+              <Field type="text" name="fullName" className="form-input" />
+              <ErrorMessage name="fullName" component="div" className="error" />
+            </div>
+            <div className="form-cadastro-perfil">
+              <label className="form-label" htmlFor="nickname">
+                Apelido
+              </label>
+              <Field type="text" name="nickname" className="form-input" />
+              <ErrorMessage name="nickname" component="div" className="error" />
+            </div>
+            <div className="form-cadastro-perfil">
+              <label className="form-label" htmlFor="sexo">
+                Sexo
+              </label>
+              <Field type="text" name="sexo" className="form-input" />
+              <ErrorMessage name="sexo" component="div" className="error" />
+            </div>
+            <div className="form-cadastro-perfil">
+              <label className="form-label" htmlFor="cyclingCategory">
+                Categoria de Ciclismo
+              </label>
+              <Field as="select" name="cyclingCategory" className="form-input">
+                <option value="AMADOR">Amador</option>
+                <option value="PROFISSIONAL">Profissional</option>
+              </Field>
+              <ErrorMessage name="cyclingCategory" component="div" className="error" />
+            </div>
+            <div className="form-cadastro-perfil">
+              <label className="form-label" htmlFor="location">
+                Localização
+              </label>
+              <Field type="text" name="location" className="form-input" />
+              <ErrorMessage name="location" component="div" className="error" />
+            </div>
+            <div className="form-cadastro-perfil">
+              <label className="form-label" htmlFor="profilePicture">
+                Foto de Perfil
+              </label>
+              <Field type="file" name="profilePicture" accept="image/jpeg, image/jpg, image/png, image/gif" className="form-input" />
+              <ErrorMessage name="profilePicture" component="div" className="error" />
+            </div>
+            <button type="submit" className="form-button">
+              Salvar Perfil
+            </button>
+          </Form>
+        </Formik>
       </div>
     </div>
   );
